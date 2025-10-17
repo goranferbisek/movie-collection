28b17506dad5ce82fa562db3de5e98f911c67567
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-10-17 10:33:14 +0200

    Style landing CTA section

--- app/page.js
+++ app/page.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={classes.content}>
+    <div className={classes.background}>
       <main className={classes.hero}>
         <h1>Search for movies and group them how ever you like.</h1>
           <div className={classes.cta}>
-              <p>Join us!</p>
-              <button>Sign up</button>
-              <span>Already have an account? <Link href="">Login</Link></span>
+              <p className={classes.join}>Join us!</p>
+              <button className={classes['signup-button']}>Sign up</button>
+              <span className={classes['login-cta']}>Already have an account? <Link href="">Login</Link></span>
           </div>
       </main>
     </div>
