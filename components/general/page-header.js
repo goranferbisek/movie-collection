a01853a845fbc686402a9176b118ce242c051fa3
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-10-18 10:09:31 +0200

    Display username in header

--- components/general/page-header.js
+++ components/general/page-header.js
@@ -1,8 +1,20 @@
 import classes from "./page-header.module.css";
+import Link from "next/link";
 
 export default function PageHeader() {
+    const authUser = null; // TODO add user auth check
+    //const authUser = {name: "John"}
+
+
     return (<header className={classes.header}>
         <div className={classes.logo}>Movie Collection</div>
-        <button className={classes.loginButton}>Login</button>
+        <div>
+            {!authUser && <button className={classes.loginButton}>Login</button> }
+            {authUser && (<>
+                <span className={classes.username}>Hi, {authUser.name}</span>
+                <Link href="">Logout</Link>
+            </>)
+            }
+        </div>
     </header>);
 }
