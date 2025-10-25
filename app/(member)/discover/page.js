b0415df8df5828104ae174e63090ce08c430898f
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-10-25 20:28:08 +0200

    Fetch movies from TMDB API

--- app/(member)/discover/page.js
+++ app/(member)/discover/page.js
@@ -1,11 +1,13 @@
 import MovieGrid from "@/components/movies/movie-grid";
-import {movies} from "@/lib/placeholder-data";
+import { getPopularMovies} from "@/lib/actions";
 
 
-export default function DiscoverPage() {
+export default async function DiscoverPage() {
+    const movies = await getPopularMovies();
+
     return (<>
         <h1>Discover</h1>
         <p>Browse for fun and exciting movies here.</p>
-        <MovieGrid movieList={movies.results} />
+        <MovieGrid movieList={movies.results}/>
     </>);
 }
