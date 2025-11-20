ca68e2fc167a656180b923c0a6a62c02f1bffedc
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-11-20 19:29:10 +0100

    Validate a user session

--- app/(member)/layout.js
+++ app/(member)/layout.js
@@ -1,12 +1,21 @@
 import "@/app/globals.css";
 import SideNavigation from "@/components/navigation/side-navigation";
+import {verifyAuth} from "@/lib/auth";
+
+import {redirect} from "next/navigation";
 
 export const metadata = {
     title: "Movie collection app",
     description: "Search for movies and group them how ever you like.",
 };
 
-export default function MemberLayout({children}) {
+export default async function MemberLayout({children}) {
+    const result = await verifyAuth();
+
+    if (!result.user) {
+        return redirect('/signup');
+    }
+
     return (<div className="content">
         <SideNavigation />
         <main className="main">
