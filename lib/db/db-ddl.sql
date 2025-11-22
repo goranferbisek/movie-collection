9c77d15162e93332796f6f9cdc36b7a300e09c22
Goran Ferbišek <goran.ferbisek@gmail.com>
2025-11-22 23:11:14 +0100

    Implement session inactivity timeout

--- lib/db/db-ddl.sql
+++ lib/db/db-ddl.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS "users" (
 
 CREATE TABLE IF NOT EXISTS "sessions" (
     id TEXT NOT NULL PRIMARY KEY,
-    secret_hash BYTEA NOT NULL, -- CHAR(?) what length
+    secret_hash BYTEA NOT NULL,
+    last_verified_at INTEGER NOT NULL, -- unix time (seconds)
     created_at INTEGER NOT NULL, -- unix time (seconds)
     user_id INTEGER NOT NULL REFERENCES users(id)
 );
